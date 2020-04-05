680f3a354870de316165ded86fc5a49a01a68810
jojo <jojojojo211@163.com>
2020-04-05 20:44:20 +0800

    bakcup

--- jojo/process_sequence.py
+++ jojo/process_sequence.py
@@ -20,18 +20,23 @@ def process_list():
             sequence = sequence.split(Sequence_1, 1)
             if sequence[1].find(Sequence_2) > 0:
                 sequence = sequence[1].split(Sequence_2, 1)
+
+                file3 = file.replace(".seq", '-jojo.seq')
+                fp = open(path_end + file3, "w")
+                fp.write(sequence[0])
+                fp.close()
+
                 if sequence[0].find(Sequence_3) > 0:
                     sequence = sequence[0].split(Sequence_3, 1)
-
-                    file1 = file.replace(".seq", '-L.seq')
-                    fp = open(path_end + file1, "w")
-                    fp.write(sequence[0])
-                    fp.close()
-
-                    file2 = file.replace(".seq", '-H.seq')
-                    fp = open(path_end + file2, "w")
-                    fp.write(sequence[1])
-                    fp.close()
+                  # file1 = file.replace(".seq", '-L.seq')
+                    # fp = open(path_end + file1, "w")
+                    # fp.write(sequence[0])
+                    # fp.close()
+                    #
+                    # file2 = file.replace(".seq", '-H.seq')
+                    # fp = open(path_end + file2, "w")
+                    # fp.write(sequence[1])
+                    # fp.close()
 
                 else:
                     print(file + "没有找到特定序列3")
