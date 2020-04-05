d3749b7e3c21e5450cc04990516b3f7eaefea567
menggod <menggod@126.com>
2020-04-05 21:04:48 +0800

    backup

--- jojo/compare.py
+++ jojo/compare.py
@@ -3,15 +3,24 @@ import os
 
 
 def diff_file():
-    path = "process_list"
+    path = "compare/"
     path_end = 'process_end_list/'
     file_old_list = os.listdir(path)
     file_new_list = os.listdir(path_end)
 
-    for index in range(0, len(file_old_list) - 1):
+    for index in range(0, len(file_old_list)):
         # print(index)
-        result = filecmp.cmp(file_old_list[index], file_new_list[index])
-        print(result)
+        file_string = open(path + "/" + file_old_list[index], "r", encoding="utf-8",
+                           errors="ignore")
+        sequence = file_string.read()
+        sequence = sequence.replace("\n", "")
+
+        file_string1 = open(path + "/" + file_new_list[index], "r", encoding="utf-8",
+                            errors="ignore")
+        sequence1 = file_string1.read()
+        sequence1 = sequence.replace("\n", "")
+
+        print(index, sequence == sequence1)
 
 
 if __name__ == '__main__':
